f6f69cc19d78a41b7186e799ef946cda11192e87
rbarbazz <42906704+rbarbazz@users.noreply.github.com>
2023-02-25 10:45:33 -0800

    feat: add tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,26 @@
 import { Layout, Text } from '@ui-kitten/components'
+import { StyledComponent } from 'nativewind'
+
+const styledTextProps = {
+  appearance: 'hint',
+  className: 'text-center',
+  component: Text,
+}
 
 export default function MainScreen() {
   return (
-    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text category="h1">Main Screen</Text>
-      <Text>
-        Change any of the text, save the file, and your app will automatically
-        update.
-      </Text>
-    </Layout>
+    <StyledComponent
+      component={Layout}
+      className="flex-1 justify-center items-center"
+    >
+      <StyledComponent component={Layout} className="m-12 space-y-6">
+        <StyledComponent {...styledTextProps}>
+          You haven't imported any contacts yet.
+        </StyledComponent>
+        <StyledComponent {...styledTextProps}>
+          Start by clicking the button in the top right corner.
+        </StyledComponent>
+      </StyledComponent>
+    </StyledComponent>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.tsx', './app/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
