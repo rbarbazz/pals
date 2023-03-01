ebad6bef2e1d94aacde8ebf6dfd4fdc13299d2f6
Raphaël Barbazza <42906704+rbarbazz@users.noreply.github.com>
2023-02-28 23:13:18 -0800

    fix: use custom safeareaview

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,9 @@ import * as eva from '@eva-design/eva'
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components'
 import { EvaIconsPack } from '@ui-kitten/eva-icons'
 import { Stack } from 'expo-router'
-import { SafeAreaView, useColorScheme } from 'react-native'
+import { useColorScheme } from 'react-native'
+
+import SafeAreaView from './components/SafeAreaView'
 
 export const unstable_settings = {
   initialRouteName: 'index',
@@ -18,7 +20,7 @@ export default function RootLayout() {
         {...eva}
         theme={colorScheme === 'dark' ? eva.dark : eva.light}
       >
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView>
           <Stack>
             <Stack.Screen name="index" options={{ headerShown: false }} />
           </Stack>
